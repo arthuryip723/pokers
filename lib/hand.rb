193341efdd9af3403bf597345ba57abf383b381e
Chris && Arthur <chris-arthur@appacademy.io>
2015-08-07 17:40:38 -0400

    add Deck#deal_new_cards and #discard

--- lib/hand.rb
+++ lib/hand.rb
@@ -5,7 +5,24 @@ class Hand
     Hand.new(deck.deal(5))
   end
 
+  attr_reader :cards
+
   def initialize(cards)
     @cards = cards
   end
+
+  def discard(card_indices)
+    discarded_cards = []
+    # card_indices.sort!.reverse!
+    card_indices.each do |index|
+      discarded_cards << cards[index]
+    end
+    discarded_cards.each { |card| cards.delete(card) }
+    discarded_cards
+  end
+
+  def deal_new_cards(cards)
+    self.cards.concat(cards)
+  end
+
 end
--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -1,9 +1,12 @@
 require 'deck'
 require 'hand'
+require 'card'
 
 describe Hand do
-  let(:hand) { Hand.new }
   let(:deck) { double(:deck) }
+  let(:hand) { Hand.deal_from(Deck.new) }
+  let(:card) { double(:card) }
+
   # describe "#initialize" do
   #   it "initializes with 5 cards" do
   #     expect(hand.cards.length).to eq(5)
@@ -13,6 +16,28 @@ describe Hand do
     it "initialized with 5 cards" do
       expect(deck).to receive(:deal).with(5)
       Hand.deal_from(deck)
+    end
+  end
+
+  describe "#discard" do
+    # it "returns n cards" do
+    #
+    # end
+
+    it "removes n cards from the hand" do
+      expect(hand.cards).to receive(:delete).exactly(2).times
+
+      hand.discard([1,4])
+    end
+  end
+
+  describe "#deal_new_cards" do
+    it "receive new cards" do
+      allow(deck).to receive(:take).with(1).and_return([card])
+      
+      hand.discard([0,1])
+      hand.deal_new_cards(deck.take(1))
+      expect(hand.cards.length).to eq(4)
     end
   end
 end
