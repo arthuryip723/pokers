196ef2c39c9a9371e2f5854b14c5031c8223fb61
Chris && Arthur <chris-arthur@appacademy.io>
2015-08-07 15:09:19 -0400

    Add suit/value validations for Card

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -7,6 +7,14 @@ describe Card do
       expect(card.suit).to be(:clubs)
       expect(card.value).to be(:king)
     end
+
+    it "raises error with invalid suit" do
+      expect { Card.new(:suedes, :king) }.to raise_error
+    end
+
+    it "raises error with invalid value" do
+      expect { Card.new(:clubs, :knave) }.to raise_error
+    end
   end
 
 end
